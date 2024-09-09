0faf13f8b5d9f2ff94404addb5416c6697f9947e
tyler <tylerpetri93@gmail.com>
2024-09-09 00:40:17 -0400

    removed postgres

--- postgresql/indexes/moc db/script.sql
+++
@@ -1,7 +0,0 @@
-DROP INDEX IF EXISTS customers_bad_idx;
-
-SELECT NOW();
-
-\COPY customers FROM 'customers.txt' DELIMITER ',' CSV HEADER;
-
-SELECT NOW();
--- postgresql/indexes/partial/careless.sql
+++
@@ -1,9 +0,0 @@
--- careless dev removed
-ALTER TABLE customers ADD PRIMARY KEY (customer_id);
-
-SELECT *
-FROM pg_Indexes
-WHERE tablename = 'customers';
-
-
---- all primary keys come with index
