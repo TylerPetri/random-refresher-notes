a32c30809f246716aafb96f018346c72d9aecb36
tyler <tylerpetri93@gmail.com>
2024-09-09 00:26:52 -0400

    normalize existing db

--- postgresql/indexes/partial/index-only.sql
+++ postgresql/indexes/partial/index-only.sql
@@ -2,7 +2,7 @@ EXPLAIN ANALYZE SELECT first_name, last_name, email_address
 FROM customers
 WHERE last_name = 'Smith';
 
--- already customers_first_name_last_name_idx, why not add email_address?
+-- already customers_first_name_last_name_idx, why not add email_address? also won't use pointer for second search
 CREATE INDEX customers_last_name_first_name_email_address_idx ON customers(last_name, first_name, email_address);
 
 EXPLAIN ANALYZE SELECT first_name, last_name, email_address
